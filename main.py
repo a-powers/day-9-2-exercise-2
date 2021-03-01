36a5d5cb9d03c0e0589ac032aba087dc2073391c
a-powers <awpowers@gmail.com>
2021-03-01 16:55:45 +0000

    update

--- main.py
+++ main.py
@@ -10,13 +10,10 @@ travel_log = [
   "cities": ["Berlin", "Hamburg", "Stuttgart"]
 },
 ]
-#🚨 Do NOT change the code above
-
-#TODO: Write the function that will allow new countries
-#to be added to the travel_log. 👇
 
 
-#🚨 Do not change the code below
+
+
 add_new_country(new_country = "Russia", new_visit = 2, new_city = ["Moscow", "Saint Petersburg"])
 print(travel_log)
 
